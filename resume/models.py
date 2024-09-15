03c1745ca4967b57c5abaa6dd1d50943a927dffc
Janardhana Reddy M S <76699907+JANARDHANAREDDYMS@users.noreply.github.com>
2024-09-15 01:30:40 -0400

    m

---
+++ resume/models.py
@@ -0,0 +1,9 @@
+# from django.db import models
+# from django.db import connection
+# # Create your models here.
+
+# class Student(models.model):
+#     name = models.CharField(max_length=64)
+#     age = models.IntegerField(max_length=2)
+#     N_Number= models.CharField(max_length=8)
+
